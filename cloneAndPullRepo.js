31d121599e20d8434272c8479a1b978e8ada50b1
Fareldeveloper869 <150409983+Fareldeveloper869@users.noreply.github.com>
2023-12-01 09:48:16 +0800

    Add files via upload

---
+++ cloneAndPullRepo.js
@@ -0,0 +1,9 @@
+import child_process from 'child_process';
+
+export default (repoExists = false, username = '', repo = '', branch = 'master') => {
+  if (!repoExists) {
+    child_process.execSync(`git clone https://${username}:${process.env.PERSONAL_ACCESS_TOKEN}@github.com/${username}/${repo}.git repos/${username}/${repo}`);
+  } else {
+    child_process.execSync(`cd repos/${username}/${repo} && git pull origin ${branch} --rebase`);
+  }
+}
